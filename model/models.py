52518c8bfd23fcab502d84d2ae536a3d705faef2
Ali Nadian <ali.nadian@geeks.ltd.uk>
2023-01-10 14:57:53 +0000

    added entity recognition code

---
+++ model/models.py
@@ -0,0 +1,12 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+# import tensorflow_addons as tfa
+from keras import applications
+# from keras.layers import Dropout, Flatten, Dense
+from tensorflow.keras.models import Sequential, Model
+
+def test(a):
+    print('hi',str(a))
+    
